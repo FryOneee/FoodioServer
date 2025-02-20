29ed5a7162a125fef2d050a2be00e20f56d4f3f6
FryOne <fryderykniedzwiecki@gmail.com>
2025-02-20 09:34:31 +0100

    gsfs

--- server.py
+++ server.py
@@ -12,14 +12,23 @@ import requests
 import json
 from jose import jwt
 from botocore.exceptions import ClientError
+import logging
+from logging.handlers import RotatingFileHandler
 
-# Konfiguracja logowania
 logger = logging.getLogger("server_logger")
 logger.setLevel(logging.INFO)
-handler = RotatingFileHandler("server.log", maxBytes=10 * 1024 * 1024, backupCount=5)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+
+# Handler do pliku
+file_handler = RotatingFileHandler("server.log", maxBytes=10 * 1024 * 1024, backupCount=5)
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+# Handler do konsoli
+console_handler = logging.StreamHandler()
+console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(console_formatter)
+logger.addHandler(console_handler)
 
 def get_secret():
     secret_name = "foodio-secrets"
